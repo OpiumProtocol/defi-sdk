2bdeacfc80c9d023f33a0487100a5fe551cbe032
Igor Sobolev <sobolev.igor@me.com>
2020-02-18 18:40:41 +0300

    updates TokenSpender

--- contracts/TokenSpender.sol
+++ contracts/TokenSpender.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.6.2;
 pragma experimental ABIEncoderV2;
 
-import { Approval } from "./Structs.sol";
+import { Approval, AmountType } from "./Structs.sol";
 import { Ownable } from "./Ownable.sol";
 import { ERC20 } from "./ERC20.sol";
 import { SafeERC20 } from "./SafeERC20.sol";
@@ -9,6 +9,8 @@ import { SafeERC20 } from "./SafeERC20.sol";
 
 contract TokenSpender is Ownable {
     using SafeERC20 for ERC20;
+
+    uint256 internal constant RELATIVE_AMOUNT_BASE = 1000;
 
     function issueAssets(
         Approval[] calldata approvals,
@@ -26,10 +28,38 @@ contract TokenSpender is Ownable {
             address asset = approval.asset;
 
             assetsToBeWithdrawn[i] = asset;
-
-            ERC20(asset).safeTransferFrom(user, msg.sender, approval.amount);
+            uint256 absoluteAmount = getAbsoluteAmount(approval, user);
+            ERC20(asset).safeTransferFrom(user, msg.sender, absoluteAmount);
         }
 
         return assetsToBeWithdrawn;
+    }
+
+    function getAbsoluteAmount(
+        Approval memory approval,
+        address user
+    )
+        internal
+        view
+        returns (uint256 absoluteAmount)
+    {
+        address asset = approval.asset;
+        AmountType amountType = approval.amountType;
+        uint256 amount = approval.amount;
+
+        require(amountType != AmountType.None, "TS: wrong amount type!");
+
+        if (amountType == AmountType.Relative) {
+            require(amount <= RELATIVE_AMOUNT_BASE, "TS: wrong relative value!");
+
+            absoluteAmount = ERC20(asset).balanceOf(user) * amount / RELATIVE_AMOUNT_BASE;
+            absoluteAmount = min(absoluteAmount, ERC20(asset).allowance(user, address(this)));
+        } else {
+            absoluteAmount = amount;
+        }
+    }
+
+    function min(uint256 x, uint256 y) internal pure returns (uint256) {
+        return x < y ? x : y;
     }
 }
